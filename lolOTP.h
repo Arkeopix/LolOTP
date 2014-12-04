2c20257f5e0ec6b120f221ba12f6e0b55287024f
Arkeopix <mendiej750@gmail.com>
2014-12-04 10:59:51 +0100

    'poil'

--- lolOTP.h
+++ lolOTP.h
@@ -1,7 +1,7 @@
 #ifndef   LOL_OTP_H_
 # define  LOL_OTP_H_
 
-# define  SECRET_DEV        "IFAU CQKC IJBE EQ2D INBU IRCE IRCU KRKF IZDE MRQ="
+# define  SECRET_DEV        "s4ai gvww cfh7 f235 tbza o5r6 gkl7 wo5r"
 # define  SECRET_SIZE       80
 # define  DECODE_BASE32_ERR "Error decoding base32 secret\n"
 
