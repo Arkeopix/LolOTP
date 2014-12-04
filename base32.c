5fd501fb801afa897bf010e827aa67c873463344
Arkeopix <mendiej750@gmail.com>
2014-12-04 17:11:42 +0100

    so close...

--- base32.c
+++ base32.c
@@ -11,30 +11,9 @@ static char *base32_alpha[32] = {
 };
 
 static  void dec_to_bin(char c, char *out) {
-  char  *padded_bin;
-
   for (int i = 7; i >= 0; --i) {
     strcat(out, (c & (1 << i)) ? "1" : "0");
   }
-}
-
-static  char *string_to_bin(char *s) {
-  char  *p = s, *bin;
-  char  tmp_bin[9];
-
-  if ((bin = malloc(1024 * sizeof(char))) == NULL) {
-    return NULL;
-  }
-  memset(bin, 0, sizeof(bin));
-  while (*p != '\0') {
-    memset(&tmp_bin, 0, sizeof(tmp_bin));
-    dec_to_bin(*p, tmp_bin);
-    printf("char %c, bin %s\n", *p, tmp_bin);
-    strcat(bin, tmp_bin);
-    p++;
-  }
-  bin[strlen(bin)] = '\0';
-  return bin;
 }
 
 static  int bin_to_dec(int bin) {
@@ -49,8 +28,76 @@ static  int bin_to_dec(int bin) {
   return dec;
 }
 
+static  char *string_to_bin(char *s) {
+  char  *p = s, *bin;
+  char  tmp_bin[9];
+
+  if ((bin = malloc(1024 * sizeof(char))) == NULL) {
+    return NULL;
+  }
+  memset(bin, 0, sizeof(bin));
+  while (*p != '\0') {
+    memset(&tmp_bin, 0, sizeof(tmp_bin));
+    dec_to_bin(*p, tmp_bin);
+    strcat(bin, tmp_bin);
+    printf("char %c, bin = %s\n", *p, tmp_bin);
+    p++;
+  }
+  bin[strlen(bin)] = '\0';
+  return bin;
+}
+
 static  char *to_base32_alpha(int chunck) {
   return base32_alpha[bin_to_dec(chunck)];
+}
+
+static  char *to_ascii(int chunck) {
+  char  *tmp;
+
+  printf("%d\n", bin_to_dec(chunck));
+  tmp[0] = bin_to_dec(chunck);
+  return tmp;
+}
+
+static  void strip_padd(char *s) {
+  char* i = s;
+  char* j = s;
+
+  while (*j != 0) {
+    *i = *j++;
+    if (*i != '=')
+      i++;
+  }
+  *i = 0;
+}
+
+static  char *get_bin_from_b32_alpha(char c) {
+  int   i = 0;
+  char  *bin;
+
+  if ((bin = malloc(9*sizeof(char))) == NULL) {
+    return NULL;
+  }
+  memset(bin, 0, sizeof(bin));
+  while (*base32_alpha[i] != c) {
+    i++;
+  }
+  dec_to_bin(i, bin);
+  printf("bin from b32 = %s\n", bin);
+  return bin;
+}
+
+int    base32_decode(char *in, char *out) {
+  char  *bin, *coded;
+  char  chunck[8];
+
+  strip_padd(in);
+  memset(bin, 0, sizeof(bin));
+  for (int i = 0; i < strlen(in); i++) {
+    strcat(bin, get_bin_from_b32_alpha(in[i]));
+  }
+  printf ("%s\n", bin);
+  return 0;
 }
 
 int     base32_encode(char *in, char *out) {
@@ -58,27 +105,32 @@ int     base32_encode(char *in, char *out) {
   char  chunck[5];
 
   bin = string_to_bin(in);
-  printf("[%s]\n", bin);
-
-  for (int i = 0; i < strlen(bin); i += 5 ) {
+  printf("full bin encode %s\n", bin);
+  for (int i = 0; i < strlen(bin); i += 5) {
     memcpy(chunck, &bin[i], 5);
     chunck[5] = '\0';
     while (strlen(chunck) % 5 != 0) {
       strcat(chunck, "0");
     }
+    printf("bin = %s\n", chunck);
     strcat(out, to_base32_alpha(atoi(chunck)));
-    printf("bin = %s, len = %d, i = %d,\t %s\n", bin, strlen(bin), i, chunck);
   }
   while (strlen(out) % 8 != 0) {
     strcat(out, "=");
   }
+  printf("%s\n", bin);
+  return 0;
 }
 
 int main() {
   char out[512];
+  char out_decode[512];
 
   memset(&out, 0, sizeof(out));
+  memset(&out_decode, 0, sizeof(out_decode));
   base32_encode("yessir", out);
   printf("out = %s\n", out);
+  base32_decode(out, out_decode);
+  printf("out %s\n", out_decode);
   return 0;
 }
